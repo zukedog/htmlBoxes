eaf7d99382ccec063f096de5181b93d29ed50f4c
Joshua SUTTON <joshua.sutton@student.ecc.sa.edu.au>
2018-03-09 15:41:00 +1030

    ?

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 // JavaScript Document
+
+$(document).ready(function(){
+	$(".win").height(window.innerHeight);
+	$(".helper").mousedown(initResize);
+	
+	
+	//.next  .prev
+});
+
+function initResize(){
+	console.log("Dragon");
+	$(this).mousemove(function(e){
+		console.log(e);
+		//console.log($(this).prev().height($(this).prev().height()+val));
+	});
+	$(this).mouseup(function(){
+		$(this).unbind("mousemove");
+	});
+}
+
+
