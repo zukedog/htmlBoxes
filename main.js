0a7a4f71e0dcd498d1144eb74a55c47130f108f6
zukedog <zukedog18@gmail.com>
2018-03-10 16:06:38 +1030

    Intitial Commit

--- main.js
+++ main.js
@@ -1,21 +1,42 @@
-// JavaScript Document
-
 $(document).ready(function(){
-	$(".win").height(window.innerHeight);
-	$(".helper").mousedown(initResize);
+	$(".win").height(window.innerHeight);//set window Height
 	
+	$(".helper[or=\"vertical\"]").on("mousedown", initResizeV);//enable vertical Helper
+	$(".helper[or=\"horizontal\"]").on("mousedown", initResizeH);
 	
-	//.next  .prev
+	$(".content[split=\"false\"]").append('<div class="tabBar"></div><div class="subTabBar"></div>');
+
+
 });
 
-function initResize(){
-	console.log("Dragon");
-	$(this).mousemove(function(e){
-		console.log(e);
-		//console.log($(this).prev().height($(this).prev().height()+val));
+function initResizeH(f){
+	var me = this;
+	var prevHeight = $(me).prev().height();
+	var nextHeight = $(me).next().height();
+	var st = f.screenY;
+	$(document).on("mousemove", function(e){
+		$(me).prev().height((prevHeight+(e.screenY-st))+"px");
+		$(me).next().height((nextHeight-(e.screenY-st))+"px");
 	});
-	$(this).mouseup(function(){
-		$(this).unbind("mousemove");
+	
+	$(document).one("mouseup", function(){
+		$(document).unbind();
+	});
+}
+
+
+function initResizeV(f){
+	var me = this;
+	var prevWidth = $(me).prev().width();
+	var nextWidth = $(me).next().width();
+	var st = f.screenX;
+	$(document).on("mousemove", function(e){
+		$(me).prev().width((prevWidth+(e.screenX-st))+"px");
+		$(me).next().width((nextWidth-(e.screenX-st))+"px");
+	});
+	
+	$(document).one("mouseup", function(){
+		$(document).unbind();
 	});
 }
 
