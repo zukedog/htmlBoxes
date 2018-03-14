2adedb390694fb9a7d6ea279b391dc9073e693c0
Joshua SUTTON <joshua.sutton@student.ecc.sa.edu.au>
2018-03-14 12:42:28 +1030

    merge vertical and horizontal resize code

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-	$(".helper[or=\"vertical\"]").on("mousedown", initResizeV);//enable vertical Helper
-	$(".helper[or=\"horizontal\"]").on("mousedown", initResizeH);
+	$(".helper").on("mousedown", initResize);//enable vertical Helper
 	
 	$(".content[split=\"false\"]").append('<div class="tabBar"></div>');//<div class="subTabBar"></div>
 	$(".content[split=\"false\"]").append('<div class="contentBody" ondrop="drop(event)" ondragenter="dragenter(event)" ondragleave="dragleave(event)" ondragover="hover(event)"></div>');
@@ -9,37 +8,28 @@ $(document).ready(function(){
 	$(".tabBar").append('<div class=tab draggable="true" ondragstart="drag(event)">tab3</div>').attr("dat",'{"Test":"bob"}');
 	$(".tabBar").append('<div class=tabFill></div>');
 	//$(".tab").attr("dat",'{"Test":"bob"}');
-	$(".contentBody").html('<div class="thinRow"><div class="side"></div><div class="middle"></div><div class="side"></div></div><div class="row"><div class="side"></div><div class="middle"></div><div class="side"></div></div><div class="thinRow"><div class="side"></div><div class="middle"></div><div class="side"></div></div>');
+	//$(".contentBody").html('<div class="thinRow"><div class="side"></div><div class="middle"></div><div class="side"></div></div><div class="row"><div class="side"></div><div class="middle"></div><div class="side"></div></div><div class="thinRow"><div class="side"></div><div class="middle"></div><div class="side"></div></div>');
 });
 
-function initResizeH(f){
+function initResize(f){
 	var me = this;
-	var prevHeight = $(me).prev().height();
-	var nextHeight = $(me).next().height();
-	var st = f.screenY;
+	var dir = "height";
+	var screen = "screenY";
+	
+	if($(me).attr("or")=="vertical"){
+		dir = "width";
+		screen = "screenX";
+	}
+	
+	var prevHeight = $(me).prev()[dir]();
+	var nextHeight = $(me).next()[dir]();
+	var st = f[screen];
 	$(document).on("mousemove", function(e){
-		$(me).prev().height((prevHeight+(e.screenY-st))+"px");
-		$(me).next().height((nextHeight-(e.screenY-st))+"px");
+		$(me).prev()[dir]((prevHeight+(e[screen]-st))+"px");
+		$(me).next()[dir]((nextHeight-(e[screen]-st))+"px");
 	});
 	
 	$(document).one("mouseup", function(){
 		$(document).unbind();
 	});
 }
-
-
-function initResizeV(f){
-	var me = this;
-	var prevWidth = $(me).prev().width();
-	var nextWidth = $(me).next().width();
-	var st = f.screenX;
-	$(document).on("mousemove", function(e){
-		$(me).prev().width((prevWidth+(e.screenX-st))+"px");
-		$(me).next().width((nextWidth-(e.screenX-st))+"px");
-	});
-	
-	$(document).one("mouseup", function(){
-		$(document).unbind();
-	});
-}
-
